5505fd7aca0fa7100591c9f9106c4f2d7cf2fad9
begumkucuk35 <35begumkucuk@gmail.com>
2025-01-04 21:05:33 +0300

    ascending and descending sort added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Cards from "./components/Cards";
 import Header from "./components/Header";
+import descendingIcon from "./assets/image/arrow-up-a-z-solid.svg";
+import ascendingIcon from "./assets/image/arrow-down-a-z-solid.svg";
 
 function App() {
   const [allCountries, setAllCountries] = useState([]);
+  const [displayedCountries, setDisplayedCountries] = useState([]);
   const [regions, setRegions] = useState([]);
   const [selectedRegion, setSelectedRegion] = useState("All");
+  const [ascending, setAscending] = useState(true);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all?")
@@ -18,13 +22,33 @@ function App() {
           ...new Set(data.map((country) => country.region)),
         ];
         setRegions(["All", ...uniqueRegions]);
+        setDisplayedCountries(
+          data.sort((a, b) =>
+            a.name.common.localeCompare(b.name.common)
+          )
+        );
       });
   }, []);
 
-  const filterCountries = () =>{
-   return selectedRegion === "All" ? allCountries : allCountries.filter((country) => country.region === selectedRegion);
-  }
-  console.log(regions);
+  
+
+  useEffect(() => {
+    const filterCountries = () => {
+      let filtered =
+        selectedRegion === "All"
+          ? [...allCountries]
+          : allCountries.filter((country) => country.region === selectedRegion);
+  console.log(ascending)
+      let sorted = ascending
+        ? filtered.sort((a, b) => a.name.common.localeCompare(b.name.common))
+        : filtered.sort((a, b) => b.name.common.localeCompare(a.name.common));
+  
+      setDisplayedCountries(sorted);
+    }; 
+    
+    filterCountries();
+  }, [selectedRegion, ascending, allCountries]);
+
   return (
     <>
       <Header />
@@ -35,9 +59,16 @@ function App() {
               {region}
             </button>
           ))}
+          <button
+            onClick={() => setAscending(!ascending)}
+            className="sort-icon"
+          >
+            
+            <img src={ascending ? ascendingIcon : descendingIcon} alt="" />
+          </button>
         </section>
         <section className="card-container">
-        {filterCountries().map((country, index) => (
+          {displayedCountries.map((country, index) => (
             <Cards
               key={index}
               name={country.name.common}
